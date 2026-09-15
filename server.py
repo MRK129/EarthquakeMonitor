8e7d8545af06fcbc4431462939de217b3f3435bb
MRK129 <kartavyaagarwal623@gmail.com>
2026-09-15 18:50:36 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from datetime import datetime
 
-app = FastAPI(title="Earthquake Monitor Server")
+app = FastAPI(
+    title="Earthquake Monitor Server"
+)
 
 latest_data = {
     "ax": 0.0,
     "ay": 0.0,
     "az": 0.0,
     "magnitude": 0.0,
+    "rms": 0.0,
+    "frequency": 0.0,
+    "amplitude": 0.0,
+    "status": "NORMAL",
+    "level": "NORMAL",
     "timestamp": None
 }
 
@@ -20,8 +27,17 @@ def home():
     }
 
 
+@app.get("/status")
+def status():
+    return {
+        "server": "online",
+        "time": datetime.now().isoformat()
+    }
+
+
 @app.post("/sensor")
 def receive_sensor_data(data: dict):
+
     global latest_data
 
     latest_data = {
@@ -29,6 +45,11 @@ def receive_sensor_data(data: dict):
         "ay": data.get("ay", 0.0),
         "az": data.get("az", 0.0),
         "magnitude": data.get("magnitude", 0.0),
+        "rms": data.get("rms", 0.0),
+        "frequency": data.get("frequency", 0.0),
+        "amplitude": data.get("amplitude", 0.0),
+        "status": data.get("status", "NORMAL"),
+        "level": data.get("level", "NORMAL"),
         "timestamp": datetime.now().isoformat()
     }
 
